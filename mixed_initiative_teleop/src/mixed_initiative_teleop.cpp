3f20d7c6ca0fd45e0af8cc4b2e9981e672bedc0b
Manolis Chiou <manolis.chiou@gmail.com>
2018-12-03 19:53:52 +0000

    husky gazebo support

--- mixed_initiative_teleop/src/mixed_initiative_teleop.cpp
+++ mixed_initiative_teleop/src/mixed_initiative_teleop.cpp
@@ -32,118 +32,123 @@
 
 /*!
 
-@mainpage
-  Joystick teleoperation node for use within the mixed initiative framework. The user can operate the robot in
-  teleoperation mode and change on the fly autonomy level/mode. Also a stop button is implimented.
-  It was ment to be used with an Xbox 360 joystick but should work with any joystick.
-<hr>
+   @mainpage
+   Joystick teleoperation node for use within the mixed initiative framework. The user can operate the robot in
+   teleoperation mode and change on the fly autonomy level/mode. Also a stop button is implimented.
+   It was ment to be used with an Xbox 360 joystick but should work with any joystick.
+   <hr>
 
-@section usage Usage
-@par    After start roscore, you need load robot configuration file to parameter server first.
+   @section usage Usage
+   @par    After start roscore, you need load robot configuration file to parameter server first.
         For example, I90 robot, you need load drrobotplayer_I90.yaml use command "rosparam load drrobotplayer_I90.yaml"
         then run drrobot_player first. then start ros joy node.
-@verbatim
-$ mixed_initiative_teleop
-@endverbatim
+   @verbatim
+   $ mixed_initiative_teleop
+   @endverbatim
 
-<hr>
-@section topic ROS topics
+   <hr>
+   @section topic ROS topics
 
-Publishes to (name / type):
--@b /teleop/cmd_vel: will publish to /teleop/cmd_vel a geometry_msgs/Twist.msg type message to drrobot_player.
- For differential robots, linear.x is forward/backward speed (m/sec), and angular.z (rad/sec)is the angular speed.
-<hr>
-*/
+   Publishes to (name / type):
+   -@b /teleop/cmd_vel: will publish to /teleop/cmd_vel a geometry_msgs/Twist.msg type message to drrobot_player.
+   For differential robots, linear.x is forward/backward speed (m/sec), and angular.z (rad/sec)is the angular speed.
+   <hr>
+ */
 
 
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include <geometry_msgs/Twist.h>
-
 #include <std_msgs/Int8.h>
 #include <std_msgs/Bool.h>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread.hpp>
 
 class JoystickTeleop
 {
 
 public:
-  JoystickTeleop();
+JoystickTeleop();
 
 private:
-  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+void  publish();
 
-  ros::NodeHandle nh_;
+ros::NodeHandle ph_,nh_;
 
-  int linear_axis_, angular_axis_, control_button_, stop_button_, auto_button_, teleop_button_;
-  double linear_scaling_, angular_scaling_;
+int linear_axis_, angular_axis_, control_button_, stop_button_, auto_button_, teleop_button_;
+double linear_scaling_, angular_scaling_;
+geometry_msgs::Twist last_msg_published_;
+boost::mutex publish_mutex_;
 
-  ros::Publisher vel_pub_, mode_pub_;
-
-  ros::Subscriber joy_sub_;
+ros::Publisher vel_pub_, mode_pub_;
+ros::Subscriber joy_sub_;
+ros::Timer timer_;
 
 };
 
 
-JoystickTeleop::JoystickTeleop()
+JoystickTeleop::JoystickTeleop() :
+        ph_("~")
+
 {
-  // Default movement axis
-  nh_.param("axis_linear", linear_axis_, 1);
-  nh_.param("axis_angular", angular_axis_, 0);
+// Default movement axis
+        ph_.param("axis_linear", linear_axis_, 1);
+        ph_.param("axis_angular", angular_axis_, 0);
 
-  // Default scaling parameters
-  nh_.param("scale_angular", angular_scaling_, 0.80);
-  nh_.param("scale_linear", linear_scaling_, 0.4);
+// Default scaling parameters
+        ph_.param("scale_angular", angular_scaling_, 0.60);
+        ph_.param("scale_linear", linear_scaling_, 0.4);
 
-  //Default buttons for Xbox 360 joystick.
-  nh_.param("teleop_button", teleop_button_, 3); // Y button
-  nh_.param("stop_button", stop_button_, 4);     // LB button
-  nh_.param("auto_button", auto_button_, 0);     // A button
+//Default buttons for Xbox 360 joystick.
+        ph_.param("teleop_button", teleop_button_, 3); // Y button
+        ph_.param("stop_button", stop_button_, 4); // LB button
+        ph_.param("auto_button", auto_button_, 0); // A button
 
+        vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/teleop/cmd_vel", 5);
+        mode_pub_ = nh_.advertise<std_msgs::Int8>("/control_mode", 5);
+        joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 2, &JoystickTeleop::joyCallback, this);
 
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/teleop/cmd_vel", 5);
-  mode_pub_ = nh_.advertise<std_msgs::Int8>("/control_mode", 5);
-
-  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 2, &JoystickTeleop::joyCallback, this);
+        timer_ = nh_.createTimer(ros::Duration(0.1), boost::bind(&JoystickTeleop::publish, this));
 
 }
 
 void JoystickTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-  geometry_msgs::Twist cmd_vel;
-  std_msgs::Int8 mode;
+        geometry_msgs::Twist cmd_vel;
+        std_msgs::Int8 mode;
 
-  // movement commands
-  cmd_vel.linear.x = linear_scaling_ * joy->axes[linear_axis_];
-  cmd_vel.angular.z = angular_scaling_ * joy->axes[angular_axis_];
+        // movement commands
+        cmd_vel.linear.x = linear_scaling_ * joy->axes[linear_axis_];
+        cmd_vel.angular.z = angular_scaling_ * joy->axes[angular_axis_];
+        last_msg_published_ = cmd_vel;
 
-  vel_pub_.publish(cmd_vel);
-
-  // autonomy mode choice
-  if (joy->buttons[stop_button_]){
-      mode.data=0;
-      mode_pub_.publish(mode);
-    }
-  if (joy->buttons[teleop_button_]){
-      mode.data=1;
-      mode_pub_.publish(mode);
-    }
-  if (joy->buttons[auto_button_]){
-      mode.data=2;
-      mode_pub_.publish(mode);
-    }
+        // autonomy mode choice
+        if (joy->buttons[stop_button_]) {
+                mode.data=0;
+                mode_pub_.publish(mode);
+        }
+        if (joy->buttons[teleop_button_]) {
+                mode.data=1;
+                mode_pub_.publish(mode);
+        }
+        if (joy->buttons[auto_button_]) {
+                mode.data=2;
+                mode_pub_.publish(mode);
+        }
 
 }
 
+void JoystickTeleop::publish()
+{
+        boost::mutex::scoped_lock lock(publish_mutex_);
+        vel_pub_.publish(last_msg_published_);
+}
 
+// the main function
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "mixed_initiative_teleop");
-  JoystickTeleop joystick_teleop;
-
-  ros::Rate r(10); // 10 hz
-  while (ros::ok())
-    {
-      ros::spinOnce();
-      r.sleep();
-    }
+        ros::init(argc, argv, "mixed_initiative_teleop");
+        JoystickTeleop joystick_teleop;
+        ros::spin();
 }
